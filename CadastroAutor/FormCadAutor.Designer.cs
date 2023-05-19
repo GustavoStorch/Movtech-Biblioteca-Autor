5899f01b4606c8bbfbdf6b8f52a7eb91013f9517
Gustavo Kauan <gustavokauan2002@hotmail.com>
2023-05-19 11:18:00 -0300

    Titulo do form e nome da classe form alteradas e icone acrescentado

---
+++ CadastroAutor/FormCadAutor.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace CadastroAutor
+{
+    partial class FormCadAutor
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCadAutor));
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblInfoAutor = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtInfoAutor = new System.Windows.Forms.TextBox();
+            this.dtgDadosAutor = new System.Windows.Forms.DataGridView();
+            this.colCodAutor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNomeAutor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDescAutor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDadosAutor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AccessibleName = "";
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(8, 46);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(55, 20);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblInfoAutor
+            // 
+            this.lblInfoAutor.AccessibleName = "";
+            this.lblInfoAutor.AutoSize = true;
+            this.lblInfoAutor.Location = new System.Drawing.Point(8, 83);
+            this.lblInfoAutor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInfoAutor.Name = "lblInfoAutor";
+            this.lblInfoAutor.Size = new System.Drawing.Size(167, 20);
+            this.lblInfoAutor.TabIndex = 2;
+            this.lblInfoAutor.Text = "Informações do Autor:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNome.BackColor = System.Drawing.SystemColors.Window;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtNome.Location = new System.Drawing.Point(200, 43);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNome.MaxLength = 50;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(648, 26);
+            this.txtNome.TabIndex = 2;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalvar.Location = new System.Drawing.Point(602, 120);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(116, 34);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExcluir.Location = new System.Drawing.Point(724, 120);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(116, 34);
+            this.btnExcluir.TabIndex = 6;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AccessibleName = "";
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(8, 9);
+            this.lblCodigo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(63, 20);
+            this.lblCodigo.TabIndex = 7;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Enabled = false;
+            this.txtCodigo.Location = new System.Drawing.Point(200, 5);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCodigo.MaxLength = 3;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(55, 26);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // txtInfoAutor
+            // 
+            this.txtInfoAutor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInfoAutor.Location = new System.Drawing.Point(200, 80);
+            this.txtInfoAutor.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtInfoAutor.MaxLength = 1000;
+            this.txtInfoAutor.Name = "txtInfoAutor";
+            this.txtInfoAutor.Size = new System.Drawing.Size(648, 26);
+            this.txtInfoAutor.TabIndex = 3;
+            // 
+            // dtgDadosAutor
+            // 
+            this.dtgDadosAutor.AllowUserToAddRows = false;
+            this.dtgDadosAutor.AllowUserToDeleteRows = false;
+            this.dtgDadosAutor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtgDadosAutor.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgDadosAutor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgDadosAutor.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colCodAutor,
+            this.colNomeAutor,
+            this.colDescAutor});
+            this.dtgDadosAutor.Location = new System.Drawing.Point(12, 165);
+            this.dtgDadosAutor.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dtgDadosAutor.MultiSelect = false;
+            this.dtgDadosAutor.Name = "dtgDadosAutor";
+            this.dtgDadosAutor.ReadOnly = true;
+            this.dtgDadosAutor.RowHeadersWidth = 62;
+            this.dtgDadosAutor.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgDadosAutor.Size = new System.Drawing.Size(837, 325);
+            this.dtgDadosAutor.TabIndex = 7;
+            this.dtgDadosAutor.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgDadosAutor_CellDoubleClick);
+            // 
+            // colCodAutor
+            // 
+            this.colCodAutor.FillWeight = 20F;
+            this.colCodAutor.HeaderText = "Código";
+            this.colCodAutor.MinimumWidth = 8;
+            this.colCodAutor.Name = "colCodAutor";
+            this.colCodAutor.ReadOnly = true;
+            // 
+            // colNomeAutor
+            // 
+            this.colNomeAutor.FillWeight = 70F;
+            this.colNomeAutor.HeaderText = "Nome";
+            this.colNomeAutor.MinimumWidth = 8;
+            this.colNomeAutor.Name = "colNomeAutor";
+            this.colNomeAutor.ReadOnly = true;
+            // 
+            // colDescAutor
+            // 
+            this.colDescAutor.HeaderText = "Info. Autor";
+            this.colDescAutor.MinimumWidth = 8;
+            this.colDescAutor.Name = "colDescAutor";
+            this.colDescAutor.ReadOnly = true;
+            // 
+            // FormCadAutor
+            // 
+            this.AcceptButton = this.btnSalvar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 495);
+            this.Controls.Add(this.dtgDadosAutor);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtInfoAutor);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblInfoAutor);
+            this.Controls.Add(this.lblNome);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimizeBox = false;
+            this.Name = "FormCadAutor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Editar Autor";
+            this.Load += new System.EventHandler(this.FormCadAutor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDadosAutor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblInfoAutor;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtInfoAutor;
+        private System.Windows.Forms.DataGridView dtgDadosAutor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCodAutor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNomeAutor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescAutor;
+    }
+}
+
---
+++ CadastroAutor/FormCadAutor.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+using static System.Windows.Forms.LinkLabel;
+using System.Runtime.CompilerServices;
+
+namespace CadastroAutor
+{
+    public partial class FormCadAutor : Form
+    {
+
+        public FormCadAutor()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCadAutor_Load(object sender, EventArgs e)
+        {
+            InitializeTable();
+            CarregaID();
+            btnExcluir.Enabled = false;
+        }
+
+        //Botão com a funcionalidade de salvar/persistir os dados inseridos no banco de dados.
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using(SqlConnection connection = DaoConnection.GetConexao())
+                {
+                    AutorDAO dao = new AutorDAO(connection);
+
+                    bool verificaCampos = dao.VerificaCampos(new AutorModel()
+                    {
+                        CodAutor = txtCodigo.Text,
+                        NomeAutor = txtNome.Text
+                    });
+
+                    if(verificaCampos)
+                    {
+                        int count = dao.VerificaRegistros(new AutorModel()
+                        {
+                            CodAutor = txtCodigo.Text
+
+                        });
+
+                        if (count > 0)
+                        {
+                            dao.Editar(new AutorModel()
+                            {
+                                CodAutor = txtCodigo.Text,
+                                NomeAutor = txtNome.Text,
+                                Descricao = txtInfoAutor.Text
+                            });
+                            MessageBox.Show("Autor atualizado com sucesso!");
+                        }
+                        else
+                        {
+                            dao.Salvar(new AutorModel()
+                            {
+                                CodAutor = txtCodigo.Text,
+                                NomeAutor = txtNome.Text,
+                                Descricao = txtInfoAutor.Text
+                            });
+                            MessageBox.Show("Autor salvo com sucesso!");
+                        }
+                    }
+                }
+
+                InitializeTable();
+                limparForm();
+                CarregaID();
+                btnExcluir.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Houve um problema ao salvar o usuário!\n{ex.Message}");
+            }
+        }
+
+        //Botão que realiza o Delete de um registro no banco de dados.
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            DialogResult conf = MessageBox.Show("Tem certeza que deseja excluir o Autor?", "Ops, tem certeza?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            try
+            {
+                if (conf == DialogResult.Yes)
+                {
+                    using (SqlConnection connection = DaoConnection.GetConexao())
+                    {
+                        AutorDAO dao = new AutorDAO(connection);
+
+                        bool verificaCampos = dao.VerificaCampos(new AutorModel()
+                        {
+                            CodAutor = txtCodigo.Text,
+                            NomeAutor = txtNome.Text
+                        });
+
+                        if (verificaCampos)
+                        {
+
+                            dao.Excluir(new AutorModel()
+                            {
+                                CodAutor = txtCodigo.Text
+                            });
+                            MessageBox.Show("Autor excluído com sucesso!");
+                        }
+                    }
+                    
+                    InitializeTable();
+                    limparForm();
+                    CarregaID();
+                    btnExcluir.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Houve um problema ao excluir o Autor!\n{ex.Message}");
+            }
+        }
+
+        //Método que limpa o formulário toda vez que é salvo ou deletado um registro.
+        public void limparForm()
+        {
+            txtCodigo.Text = String.Empty;
+            txtInfoAutor.Text = String.Empty;
+            txtNome.Text = String.Empty;
+
+        }
+
+        //Carrega todos os registros contidos no banco de dados para a DataGridView.
+        private void InitializeTable()
+        {
+            dtgDadosAutor.Rows.Clear();
+            using(SqlConnection connection = DaoConnection.GetConexao())
+            {
+                AutorDAO dao = new AutorDAO(connection);
+                List<AutorModel> autores = dao.GetAutores();
+                foreach(AutorModel autor in autores) {
+                    DataGridViewRow row = dtgDadosAutor.Rows[dtgDadosAutor.Rows.Add()];
+                    row.Cells[colCodAutor.Index].Value = autor.CodAutor;
+                    row.Cells[colNomeAutor.Index].Value = autor.NomeAutor;
+                    row.Cells[colDescAutor.Index].Value = autor.Descricao;
+                }
+            }
+        }
+
+        //Recupera o próximo id a ser cadastrado e joga ele para o textBox.
+        private void CarregaID()
+        {
+            using (SqlConnection connection = DaoConnection.GetConexao())
+            {
+
+                SqlCommand cm = new SqlCommand("SELECT IDENT_CURRENT('mvtBibAutor') + 1", connection);
+                int nextCod = Convert.ToInt32(cm.ExecuteScalar());
+
+                txtCodigo.Text = nextCod.ToString();
+            }
+        }
+
+        //Método que realiza o double click em uma linha da grid e joga todos os seus dados para as textBox.
+        private void dtgDadosAutor_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex > -1 && e.ColumnIndex > -1)
+            {
+                txtCodigo.Text = dtgDadosAutor.Rows[e.RowIndex].Cells[colCodAutor.Index].Value + "";
+                txtNome.Text = dtgDadosAutor.Rows[e.RowIndex].Cells[colNomeAutor.Index].Value + "";
+                txtInfoAutor.Text = dtgDadosAutor.Rows[e.RowIndex].Cells[colDescAutor.Index].Value + "";
+
+                if (string.IsNullOrEmpty(this.txtNome.Text))
+                {
+                    btnExcluir.Enabled = false;
+                    CarregaID();
+                }
+                else
+                {
+                    btnExcluir.Enabled = true;
+                }
+            }
+        }
+    }
+}
